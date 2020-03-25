f78967a391fd2501c5bc28a6eb1755c55073716c
Patrik Henriksson <pate.henriksson@gmail.com>
2020-03-25 13:43:30 +0200

    Added heroku DB support

--- application/__init__.py
+++ application/__init__.py
@@ -4,8 +4,13 @@ app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///recipes.db"
-app.config["SQLALCHEMY_ECHO"] = True
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///recipes.db"
+    app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
@@ -36,4 +41,7 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 # Luodaan taulut
-db.create_all()
+try:
+    db.create_all()
+except:
+    pass
